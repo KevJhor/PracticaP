b3fd44220bac1313dbc1204aed89a22606139f91
K <Kevjhor@gmail.com>
2023-11-05 18:59:48 -0500

    Add PC2

--- app/src/main/java/com/example/practicap/MainActivity.kt
+++ app/src/main/java/com/example/practicap/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,20 +16,43 @@ class MainActivity : AppCompatActivity() {
 
         val user: EditText = findViewById(R.id.txtUser)
         val pass: EditText = findViewById(R.id.txtPass)
+        val auth = FirebaseAuth.getInstance()
+        val btnR: Button = findViewById(R.id.btnRegister)
+
+
 
 
         val btnLog: Button = findViewById(R.id.btnLog)
 
+        btnR.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity ::class.java))
+
+        }
+
+
+
         btnLog.setOnClickListener {
-            if(user.text.toString() == "admin@smartapp.com" && pass.text.toString() == "admin123"){
-                val intent = Intent(this,OpcionesActivity::class.java)
-                startActivity(intent)
-            }
-            else{
-                Toast.makeText(this,"Correo y/o clave inválida",Toast.LENGTH_SHORT).show()
-            }
-            /*val intent = Intent(this,OpcionesActivity::class.java)
-            startActivity(intent)*/
+            val correo: String = user.text.toString()
+            val clave: String =  pass.text.toString()
+            auth.signInWithEmailAndPassword(correo, clave)
+                .addOnCompleteListener(this){task->
+                    if(task.isSuccessful){
+                        Snackbar
+                            .make(
+                                findViewById(android.R.id.content)
+                                ,"Inicio de sesión exitoso"
+                                , Snackbar.LENGTH_LONG
+                            ).show()
+                        startActivity(Intent(this, OpcionesActivity ::class.java))
+                    }else {
+                        Snackbar
+                            .make(
+                                findViewById(android.R.id.content)
+                                ,"Credenciales inválidas"
+                                , Snackbar.LENGTH_LONG
+                            ).show()
+                    }
+                }
         }
     }
 
---
+++ app/src/main/java/com/example/practicap/model/UserModel.kt
@@ -0,0 +1,10 @@
+package com.example.practicap.model
+
+data class UserModel(
+    val email: String="",
+    val password: String = "",
+    val fullName: String = "",
+    val country: String= "",
+    val UID: String= ""
+)
+
